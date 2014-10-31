a9397c68af617ec99e3695ec08223a2ee3f32acd
danne <daniel.karling@gmail.com>
2014-10-31 22:55:38 +0000

    Fixed stuff

--- www/git_pull.php
+++ www/git_pull.php
@@ -12,5 +12,5 @@ file_put_contents('../logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
 if($payload->ref === 'refs/heads/master')
 {
-	exec('./update.sh');
+	exec('cd .. && ./update.sh');
 }
