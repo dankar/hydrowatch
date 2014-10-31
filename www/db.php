c65db957637dbb405ad21d96a3b6360227eb94f8
danne <daniel.karling@gmail.com>
2014-10-31 19:50:04 +0000

    Basic web interface and db handling

--- www/db.php
+++ www/db.php
@@ -6,6 +6,39 @@ class sensor_db extends SQLite3
 	{
 		$this->open('../db/sensors.db');
 	}
+
+	function get_data($num, $table)
+	{
+		$sql = "SELECT * FROM (SELECT value, timestamp FROM " .  $table . " ORDER BY timestamp DESC LIMIT " . $num . ") sub ORDER BY timestamp ASC";
+		$ret = $this->query($sql);
+		$response = "";
+		$first = true;
+		while($row = $ret->fetchArray(SQLITE3_ASSOC))
+		{
+			$data = "[" . strtotime($row["timestamp"]) . ", " . $row["value"] . "]";
+			if($first)
+			{
+				$response = $response . $data;
+				$first = false;
+			}
+			else
+			{
+				$response = $response . ", " . $data;
+			}
+		}
+
+		return $response;
+	}
+
+	function get_day($table)
+	{
+		return $this->get_data(8640, $table);
+	}
+
+	function get_hours($table)
+	{
+		return $this->get_data(360, $table);
+	}
 }
 
 ?>
--- www/index.php
+++ www/index.php
@@ -1,10 +1,67 @@
 <?
 include('db.php');
+
 $db = new sensor_db();
+
 if(!$db)
 {
 	echo $db->lastErrorMsg();
-} else {
- 	echo "Opened!";
 }
+
 ?>
+<html>
+	<head>
+		<title>Hydrowatch</title>
+		<script src="js/grapho.js"></script>
+	</head>
+	<body style="background-color: #252420; color: #CACACA; font-size: 12px; font-family: 'Droid Sans';">
+		<div id="day_level" style="margin-top: 50px; margin-left: 50px; width: 500; height: 300;">
+			<span><h2>Water level</h2></span>
+			<script>
+				var day_level_graph = new Grapho({place: 'day_level'});
+				day_level_graph.addDataset({
+					type:'line',
+					strokeStyle: '#F0F066',
+					y: {
+						scaleStyle: '#F0F066',
+						name: "Level",
+						showGridLines: true,
+						showLabels: true,
+						max: 1024,
+						min: 100
+					},
+					x: {
+						showLabels: true,
+						labelRotation: 45,
+						labelFormat: Grapho.formats.time
+					},
+					data: [<? echo $db->get_day("water_level"); ?>]
+				});
+			</script>
+		</div>
+		<div id="day_temperature" style="margin-top: 50px; margin-left: 50px; width: 500; height: 300;">
+                        <span><h2>Water temperature</h2></span>
+                        <script>
+                                var day_temperature_graph = new Grapho({place: 'day_temperature'});
+                                day_temperature_graph.addDataset({
+                                        type:'line',
+                                        strokeStyle: '#F0F066',
+                                        y: {
+                                                scaleStyle: '#F0F066',
+                                                showGridLines: true,
+                                                showLabels: true,
+                                                name: "Centigrades",
+						min: 15,
+						max: 50
+                                        },
+                                        x: {
+                                                showLabels: true,
+                                                labelRotation: 45,
+                                                labelFormat: Grapho.formats.time
+                                        },
+                                        data: [<? echo $db->get_day("water_temperature"); ?>]
+                                });
+                        </script>
+                </div>
+	</body>
+</html>
