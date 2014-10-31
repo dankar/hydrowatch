eae0fce06f0510feeb12244d996db5ceded3e2dc
danne <daniel.karling@gmail.com>
2014-10-31 22:51:36 +0000

    Added webhook

---
+++ www/git_pull.php
@@ -0,0 +1,16 @@
+<?
+try
+{
+	$payload = json_decode($_REQUEST['payload']);
+}
+catch(Exception $e)
+{
+	exit(0);
+}
+
+file_put_contents('../logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+
+if($payload->ref === 'refs/heads/master')
+{
+	exec('./update.sh');
+}
