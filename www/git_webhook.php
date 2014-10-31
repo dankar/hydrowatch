e3fa60f8a3022fdc1d4fa3f365a623a4e19c5616
danne <daniel.karling@gmail.com>
2014-10-31 23:39:28 +0000

    Test

--- www/git_webhook.php
+++ www/git_webhook.php
@@ -12,7 +12,7 @@ file_put_contents('../logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 
 if($payload->ref === 'refs/heads/master')
 {
-	$command = "git -C ../ pull 2>&1 > /tmp/log";
+	$command = "cd ../ && git pull 2>&1 > /tmp/log";
 	file_put_contents('../logs/github.txt', "RUNNING GIT PULL FROM!\n" . $command, FILE_APPEND);
 
 	file_put_contents('../logs/github.txt', shell_exec($command), FILE_APPEND);
