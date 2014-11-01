844727d741c2ad8ec86a5fb4f29ae6fcbaf623c5
Hexagon <robinnilsson@gmail.com>
2014-11-01 16:58:38 +0100

    db cache

--- www/db.php
+++ www/db.php
@@ -1,5 +1,4 @@
-<?
-
+<?php
 class sensor_db extends SQLite3
 {
 	function __construct()
@@ -7,16 +6,38 @@ class sensor_db extends SQLite3
 		$this->open('../db/sensors.db');
 	}
 
-	function get_data($num, $table)
-	{
-		// TODO: better query
-		$sql = "SELECT 0+strftime('%s',timestamp), value FROM " .  $table . " WHERE timestamp > date('now','localtime','-" . $num . " seconds') ORDER BY timestamp DESC";
-		$ret = $this->query($sql);
-		$response = array();
-		while($row = $ret->fetchArray(SQLITE3_NUM)){ 
-			$response[] = $row;
+	function get_cache($num, $table) {
+		$cache_file = "cache/$num$table.cache";
+		$cache_life = 30;
+		if (!file_exists($cache_file) or (time() - filemtime($cache_file) >= $cache_life)){
+			return "";
+		} else {
+			return file_get_contents($cache_file);
 		}
+	}
 
+	function save_cache($num, $table, $response) {
+		file_put_contents("cache/$num$table.cache",$response);
+	}
+
+	function get_data($num, $table, $use_cache = true)
+	{
+		
+		$response = "";
+		if ($use_cache) {
+			$response = get_cache($num, $table);
+		}
+		if ($response == "") {
+			// TODO: better query
+			$sql = "SELECT 0+strftime('%s',timestamp), value FROM " .  $table . " WHERE timestamp > date('now','localtime','-" . $num . " seconds') ORDER BY timestamp DESC";
+			$ret = $this->query($sql);
+			$response = array();
+			while($row = $ret->fetchArray(SQLITE3_NUM)){ 
+				$response[] = $row;
+			}
+			$response = json_encode($response);
+			save_cache($num, $table, $response);
+		}
 		return $response;
 	}
 
