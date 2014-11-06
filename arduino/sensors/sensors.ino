eb2f9fd51f88473c1b05f1657a44f242b6ebaa85
Daniel Karling <daniel.karling@gmail.com>
2014-11-06 20:07:24 +0100

    New arduino code

--- arduino/sensors/sensors.ino
+++ arduino/sensors/sensors.ino
@@ -2,7 +2,8 @@
 #include <OneWire.h>
 
 #define ONE_WIRE_PIN 2
-#define WATER_LEVEL_PIN
+#define WATER_LEVEL_PIN 0
+#define LIGHT_LEVEL_PIN 4
 
 OneWire one_wire(ONE_WIRE_PIN);
 DallasTemperature sensor(&one_wire);
@@ -10,19 +11,25 @@ DallasTemperature sensor(&one_wire);
 void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("start");
   sensor.begin();
 }
 
 void loop(void)
 {
-  float water_temperature, water_level;
+  while(!Serial.available() > 0);
+  while(Serial.available() > 0) Serial.read();
+  float water_temperature, water_level, light_level;
   sensor.requestTemperatures();
   water_temperature = sensor.getTempCByIndex(0);
-  water_level = analogRead(0);
-  Serial.print("temp: ");
-  Serial.println(water_temperature);
-  Serial.print("level: ");
-  Serial.println(water_level);
+  water_level = analogRead(WATER_LEVEL_PIN);
+  light_level = analogRead(LIGHT_LEVEL_PIN);
+  Serial.print("water_temperature: ");
+  Serial.print(water_temperature);
+  Serial.print("\n");
+  Serial.print("water_level: ");
+  Serial.print(water_level);
+  Serial.print("\n");
+  Serial.print("light_level: ");
+  Serial.print(light_level);
   Serial.print("\n\n"); // Two newlines marks end of packet
 }
