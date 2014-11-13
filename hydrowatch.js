325c83b531b37cc800ce9cec7477839e2c628c6b
Daniel Karling <daniel.karling@gmail.com>
2014-11-13 17:39:39 +0100

    Added states and commands

--- hydrowatch.js
+++ hydrowatch.js
@@ -5,22 +5,27 @@ var datasource = require('./datasource.js');
 
 var io;
 
-var num_history = 0;
-
 function get_history(table)
 {
-	//var sql = "SELECT strftime('%s',timestamp) as ts, value FROM " + table + " WHERE timestamp > datetime('now",'localtime','86400 seconds') ORDER BY timestamp DESC';
-
-	var sql = "SELECT strftime('%s',timestamp) as ts, value FROM " + table + " ORDER BY timestamp DESC";
-
-	var result = [];
+	var sql = "SELECT strftime('%s',timestamp) as ts, value FROM " + table + " WHERE timestamp > datetime('now','localtime','-86400 seconds') ORDER BY timestamp DESC";
 
 	datasource.db.all(sql,
 		function(err, data){
 			
 			if(!err)
 			{
-				io.sockets.emit('message', {msg: 'history', data: { name: table, data: data.map(function(row){ return [ row.ts, row.value];}) }});
+				io.sockets.emit('message', {
+						msg: 'history', 
+						data: 
+							{ 
+								name: table, 
+								data: data.map(
+									function(row){ 
+										return [row.ts, row.value];
+									}) 
+								}
+							}
+						);
 			}
 		});
 }
@@ -37,6 +42,32 @@ function get_current(table)
 		});
 }
 
+function get_states()
+{
+	var sql = "SELECT name, value FROM states";
+	
+	datasource.db.all(sql,
+		function(err, data){
+			if(!err)
+			{
+				io.sockets.emit('message', {
+						msg: 'states',
+						data:
+							{
+								name: 'states',
+								data: data.map(
+									function(row){
+										return [row.name, row.value];
+									})
+								}
+						}
+
+						);
+			}
+		});
+}
+						
+
 function send_data(socket)
 {
 	get_current('water_temperature');
@@ -46,14 +77,30 @@ function send_data(socket)
 	get_history('water_temperature');
 	get_history('water_level');
 	get_history('light_level');
+	
+	get_states();
+}
+
+function post_command(cmd)
+{
+	var sql = "INSERT INTO commands (command_string) VALUES ('" + cmd + "')";
+	datasource.db.run(sql);
 }
 	
+
+function parse_message(msg)
+{
+	if(msg.msg == 'set-light')
+	{
+		post_command('set-light ' + msg.data);
+	}
+}
 
 datasource.Init(function(){
 	io = socketio.listen(server).set('log level', 0);
 	io.on('connection', function(socket){
 			send_data(socket);
-			socket.on('message', function(msg) { console.log(msg); });
+			socket.on('message', parse_message);
 			socket.on('error', function(err){ console.log(err); });
 	});
 
@@ -62,7 +109,7 @@ datasource.Init(function(){
 function updater()
 {
 	send_data(io.sockets);
-	setTimeout(updater, 1000);
+	setTimeout(updater, 30000);
 }
 
 updater();
---
+++ www/js/graphs.js
@@ -0,0 +1,95 @@
+
+var datasets = {}
+var graphs = {}
+
+var water_temperature_graph = {
+					type:'line',
+					strokeStyle: '#F0F066',
+					y: {
+							scaleStyle: '#F0F066',
+							showGridLines: true,
+							labels: true,
+							name: "Temperature (C)",
+							nameFont: '13px Droid Sans', 
+							min: 17,
+							max: 27
+					},
+					x: {
+						showGridLines: true,
+						gridStyle: '#221010',
+							labels: true,
+							labelRotation: 45,
+							labelFormat: Grapho.formats.time
+					},
+					data: []
+			};
+			
+var water_level_graph = {
+		type:'line',
+		strokeStyle: '#6666F0',
+		y: {
+						axis: 2,
+			scaleStyle: '#6666F0',
+			showScale: true,
+			name: "Level",
+			nameFont: '13px Droid Sans',
+			labels: true,
+			max: 1024,
+			min: 0
+		},
+				data: []
+	};
+	
+var light_level_graph = {
+							type:'line',
+							strokeStyle: '#F0F066',
+							y: {
+									scaleStyle: '#F0F066',
+									showGridLines: true,
+									labels: true,
+									name: "Light",
+									nameFont: '13px Droid Sans',
+									//                              min: 17,
+									//                              max: 25
+							},
+							x: {
+									showGridLines: true,
+									gridStyle: '#221010',
+									labels: true,
+									labelRotation: 45,
+									labelFormat: Grapho.formats.time
+							},
+							data: []
+					};
+
+function create_graph(place)
+{
+	graphs[place] = new Grapho({place: place});
+}
+
+function update_graph(graph, dataset, data)
+{
+	if(graphs[graph] == undefined)
+	{
+		console.log("No such graph " + graph);
+		return;
+	}
+		
+	if(datasets[dataset] !== undefined)
+	{
+		graphs[graph].removeDataset(datasets[dataset]);
+	}
+	
+	var graph_add;
+	
+	if(dataset == 'water_temperature')
+		graph_add = water_temperature_graph;
+	else if(dataset == 'water_level')
+		graph_add = water_level_graph;
+	else if(dataset == 'light_level')
+		graph_add = light_level_graph;
+		
+	graph_add.data = data;
+	
+	datasets[dataset] = graphs[graph].addDataSet(graph_add);
+}
